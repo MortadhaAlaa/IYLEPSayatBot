70e3efd8997bc897a7166b7f77fed0831a63e9b5
Mortadha Alaa <mortadha.alaa@gmail.com>
2018-01-29 01:42:20 +0300

    real fix

--- bot.py
+++ bot.py
@@ -9,8 +9,6 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='مرحبا ب بوت السايات الخاص بلايلب..🔥\nاكتب السؤال الي تحب تسألة ، حيوصل للالومناي بصورة مجهولة! \nويتم الرد عليه باقرب وقت بلقناة❤️')
 
 def message(bot, update):
-    if update.message.chat_id in ('-273378117', '-253860529'):
-        return
     bot.send_message(chat_id='-273378117', text=update.message.text)
     bot.send_message(chat_id=update.message.chat_id, text='تم ارسال رسالتك بصورة مجهولة،\nانتظر الجواب قريبا على القناة ❤️..')
     bot.forward_message(chat_id='-253860529', from_chat_id=update.message.chat_id, message_id=update.message.message_id)
@@ -20,7 +18,7 @@ def error(bot, update, error):
 u = Updater(os.environ['TELEGRAM_TOKEN'])
 d = u.dispatcher
 d.add_handler(CommandHandler('start', start))
-d.add_handler(MessageHandler(Filters.text, message))
+d.add_handler(MessageHandler(Filters.text|Filters.private, message))
 d.add_error_handler(error)
 
 u.start_polling()
